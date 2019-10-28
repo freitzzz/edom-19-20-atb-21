75646847ad279152642ce30a54240618e06d2757
freitzzz <1160907@isep.ipp.pt>
2019-10-28 13:33:42 +0000

    Added custom workflow component that generated a PUML diagram of the DSL
    see #22

---
+++ freitas/increment2/requirements.dsl.parent/requirements.dsl/src/requirements/javacode/workflow/GeneratePUMLDiagram.java
@@ -0,0 +1,52 @@
+package requirements.javacode.workflow;
+
+import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowContext;
+
+import requirements.javacode.GenerateRequirementsPUMLUsingCircles;
+import requirements.javacode.GenerateRequirementsPUMLUsingObjectDiagram;
+
+public class GeneratePUMLDiagram extends WorkflowComponentWithSlot {
+
+	private String pumlDiagramType;
+
+	private String generatedPUMLDiagramFilePath;
+
+	@Override
+	public void invoke(IWorkflowContext ctx) {
+		
+		String dslFilePath = getSlot();
+		
+		String[] arguments = new String[] {dslFilePath, generatedPUMLDiagramFilePath};
+		
+		switch(pumlDiagramType.toLowerCase()) {
+		case "circles":
+			GenerateRequirementsPUMLUsingCircles.main(arguments);
+			break;
+		case "object":
+			GenerateRequirementsPUMLUsingObjectDiagram.main(arguments);
+			break;
+		default:
+			throw new IllegalStateException("'" + pumlDiagramType + "' is not a valid puml diagram type (can either be circles or object)");
+		}
+		
+		
+		
+	}
+
+	public String getPumlDiagramType() {
+		return pumlDiagramType;
+	}
+
+	public void setPumlDiagramType(String pumlDiagramType) {
+		this.pumlDiagramType = pumlDiagramType;
+	}
+
+	public String getGeneratedPUMLDiagramFilePath() {
+		return generatedPUMLDiagramFilePath;
+	}
+
+	public void setGeneratedPUMLDiagramFilePath(String generatedPUMLDiagramFilePath) {
+		this.generatedPUMLDiagramFilePath = generatedPUMLDiagramFilePath;
+	}
+
+}

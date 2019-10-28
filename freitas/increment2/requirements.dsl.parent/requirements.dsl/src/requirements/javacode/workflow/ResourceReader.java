415acdccc831dbda1545a17ef15a4a34b94f1c2a
freitzzz <1160907@isep.ipp.pt>
2019-10-28 00:59:56 +0000

    Added workflow component that reads a requirements dsl file
    and stores it in workflow memory
    
    see #22

---
+++ freitas/increment2/requirements.dsl.parent/requirements.dsl/src/requirements/javacode/workflow/ResourceReader.java
@@ -0,0 +1,43 @@
+package requirements.javacode.workflow;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowContext;
+import org.eclipse.xtext.resource.XtextResourceSet;
+
+import com.google.inject.Injector;
+
+import requirements.RequirementsFactory;
+import requirements.RequirementsPackage;
+import requirements.dsl.RDSLStandaloneSetup;
+
+public class ResourceReader extends WorkflowComponentWithSlot {
+	private String uri;
+
+	public void invoke(IWorkflowContext ctx) {
+		// do this only once per application
+		Injector injector = new RDSLStandaloneSetup().createInjectorAndDoEMFRegistration();
+
+		// obtain a resourceset from the injector
+		XtextResourceSet resSet = injector.getInstance(XtextResourceSet.class);
+
+		// Initialize the model
+		RequirementsPackage.eINSTANCE.eClass();
+
+		// Retrieve the default factory singleton
+		RequirementsFactory factory = RequirementsFactory.eINSTANCE;
+
+		// ResourceSet resourceSet = new ResourceSetImpl();
+		URI fileURI = URI.createFileURI(uri);
+		Resource resource = resSet.getResource(fileURI, true);
+		ctx.put(getSlot(), resource);
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+}

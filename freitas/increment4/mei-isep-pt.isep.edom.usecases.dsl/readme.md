62ee8f0f0b99debec0c046cfc6f6e0d74ad2c8a6
freitzzz <1160907@isep.ipp.pt>
2019-11-23 14:17:28 +0000

    Added base use case projects for increment 4
    see #25

---
+++ freitas/increment4/mei-isep-pt.isep.edom.usecases.dsl/readme.md
@@ -0,0 +1,7 @@
+# Use Case DSL Project
+
+This is an Xtext Eclipse project that implements a Use Case DSL.
+	
+# Note
+
+- Noting yet...
---
+++ freitas/increment4/mei-isep-pt.isep.edom.usecases2/src-gen/pt/isep/edom/usecases/impl/UsecasesFactoryImpl.java
@@ -0,0 +1,150 @@
+/**
+ */
+package pt.isep.edom.usecases.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+import pt.isep.edom.usecases.*;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class UsecasesFactoryImpl extends EFactoryImpl implements UsecasesFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static UsecasesFactory init() {
+		try {
+			UsecasesFactory theUsecasesFactory = (UsecasesFactory)EPackage.Registry.INSTANCE.getEFactory(UsecasesPackage.eNS_URI);
+			if (theUsecasesFactory != null) {
+				return theUsecasesFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new UsecasesFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public UsecasesFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case UsecasesPackage.USE_CASE_MODEL: return createUseCaseModel();
+			case UsecasesPackage.ACTOR: return createActor();
+			case UsecasesPackage.ASSOCIATION: return createAssociation();
+			case UsecasesPackage.USE_CASE: return createUseCase();
+			case UsecasesPackage.EXTEND: return createExtend();
+			case UsecasesPackage.INCLUDE: return createInclude();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public UseCaseModel createUseCaseModel() {
+		UseCaseModelImpl useCaseModel = new UseCaseModelImpl();
+		return useCaseModel;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Actor createActor() {
+		ActorImpl actor = new ActorImpl();
+		return actor;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Association createAssociation() {
+		AssociationImpl association = new AssociationImpl();
+		return association;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public UseCase createUseCase() {
+		UseCaseImpl useCase = new UseCaseImpl();
+		return useCase;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Extend createExtend() {
+		ExtendImpl extend = new ExtendImpl();
+		return extend;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Include createInclude() {
+		IncludeImpl include = new IncludeImpl();
+		return include;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public UsecasesPackage getUsecasesPackage() {
+		return (UsecasesPackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static UsecasesPackage getPackage() {
+		return UsecasesPackage.eINSTANCE;
+	}
+
+} //UsecasesFactoryImpl
